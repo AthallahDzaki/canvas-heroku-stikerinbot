462e499526cdb9d2a788d92c7b7d9f0e50e41bc7
AthallahDzaki <AthallahDzaki@gmail.com>
2021-09-01 16:45:59 +0700

    Remove Unused File And Modifed TBC

--- module/tbc.js
+++ module/tbc.js
@@ -1,5 +1,14 @@
 const { createCanvas, loadImage } = require('canvas');
-const { drawImageWithTint } = require('./../lib/Canvas');
+
+drawImageWithTint(ctx, image, color, x, y, width, height) {
+    const { fillStyle, globalAlpha } = ctx;
+    ctx.fillStyle = color;
+    ctx.drawImage(image, x, y, width, height);
+    ctx.globalAlpha = 0.5;
+    ctx.fillRect(x, y, width, height);
+    ctx.fillStyle = fillStyle;
+    ctx.globalAlpha = globalAlpha;
+}
 
 async function draw(avatarurl) {
     const base = await loadImage("https://raw.githubusercontent.com/bot-clones/xiao/master/assets/images/to-be-continued.png");
